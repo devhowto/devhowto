7a409b1860a40301c4adc50c5b0eb92a99d0f110
Fernando Basso <fernandobasso.br@gmail.com>
2024-03-12 07:21:45 -0300

    Copy assets to build dir

--- copy-assets.sh
+++ copy-assets.sh
@@ -4,5 +4,5 @@ assets_dir=($(find ./docs -type d -name __assets -print))
 
 for asset_dir in "${assets_dir[@]}"
 do
-  cp -rv "$asset_dir" "${asset_dir/docs/public}"
+  cp -rv "$asset_dir" "${asset_dir/docs/build}"
 done
