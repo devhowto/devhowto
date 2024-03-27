e2ee0b69cd3f497741580ab25d50c64e403cd285
Fernando Basso <fernandobasso.br@gmail.com>
2024-03-27 06:23:38 -0300

    Add sitemap generation bash script

--- copy-assets.sh
+++ copy-assets.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+##
+# Create an array with all __asset directories.
+#
 assets_dir=($(find ./docs -type d -name __assets -print))
 
+##
+# Copy assets to build directory for each category.
+#
 for asset_dir in "${assets_dir[@]}"
 do
   cp -rv "$asset_dir" "${asset_dir/docs/build}"
 done
 
+##
+# Copy google verification to build directory.
+#
 cp -v ./google4e1c1c5cc9907ad4.html ./build
+
+##
+# Generate simple sitemap.txt file on build directory.
+#
+printf '%s\n' ./build/**/*.html \
+  | sed 's@^\./build/@https:\/\/devhowto.gitlab.io/@' \
+  1> ./build/sitemap.txt
