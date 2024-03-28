81d1e8530c0da48e18097f1dfaede2b763cc0913
Fernando Basso <fernandobasso.br@gmail.com>
2024-03-28 08:31:17 -0300

    feat: Produce sitemap and commit it with repository
    • Generate sitemap.txt at root directory of project.
    • Update script to also copy sitemap.txt to build/ directory.

--- copy-assets.sh
+++ copy-assets.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#
+# This script must be run _after_ the main building of the pages
+# with AdocWiki is performed and the build/ directory contains
+# all the generated HTML pages.
+#
+
 ##
 # Create an array with all __asset directories.
 #
@@ -14,14 +20,16 @@ do
 done
 
 ##
-# Copy google verification to build directory.
-#
-cp -v ./google4e1c1c5cc9907ad4.html ./build
-cp -v ./robots.txt ./build
-
-##
 # Generate simple sitemap.txt file on build directory.
 #
 printf '%s\n' ./build/**/*.html \
   | sed 's@^\./build/@https:\/\/devhowto.gitlab.io/@' \
-  1> ./build/sitemap.txt
+  1> ./sitemap.txt
+
+cp -v ./sitemap.txt ./build
+
+##
+# Copy google verification to build directory.
+#
+cp -v ./google4e1c1c5cc9907ad4.html ./build
+cp -v ./robots.txt ./build
