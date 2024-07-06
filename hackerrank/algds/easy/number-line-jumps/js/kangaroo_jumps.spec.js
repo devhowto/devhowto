f50fe54e57253fdfc56a5f77179d9a4403c2eccd
Fernando Basso <fernandobasso.br@gmail.com>
2024-07-06 08:01:01 -0300

    hackerrank(algds): Number jump line (kangaroo) v1

--- hackerrank/algds/easy/number-line-jumps/js/kangaroo_jumps.spec.js
+++ hackerrank/algds/easy/number-line-jumps/js/kangaroo_jumps.spec.js
@@ -8,4 +8,9 @@ describe("kangaroo", () => {
   test("same start location and different jump velocity", () => {
     expect(kangaroo(0, 1, 0, 2)).toBe("NO");
   });
+
+  test("same location but different velocities", () => {
+    expect(kangaroo(0, 1, 0, 2)).toBe("NO");
+    expect(kangaroo(0, 2, 0, 1)).toBe("NO");
+  });
 });
--- hackerrank/algds/easy/number-line-jumps/js/kangaroo_jumps_v1.js
+++ hackerrank/algds/easy/number-line-jumps/js/kangaroo_jumps_v1.js
@@ -1,8 +1,17 @@
-function kangaroo(x1, v1, x2, v2) {
-  let jumpX1 = x1,
-      jumpX2 = x2;
+const MAX_POSITION_DEFAULT = 1e4;
 
+function kangaroo(x1, v1, x2, v2, MAX_POS = MAX_POSITION_DEFAULT) {
+  let p1 = x1 + v1,
+      p2 = x2 + v2;
 
+  while (p1 <= MAX_POS && p2 <= MAX_POS) {
+    if (p1 === p2) return "YES";
+
+    p1 += v1;
+    p2 += v2;
+  }
+
+  return "NO";
 }
 
 export { kangaroo };
