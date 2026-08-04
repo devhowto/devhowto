e40b35a7e66e65358147c1fe01e9658fd563f58e
Fernando Basso <fernandobasso.br@gmail.com>
2026-08-04 08:00:15 -0300

    chore: Add watch.sh script

---
+++ watch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+##
+# $ sudo pacman --sync --refresh --needed inotify-tools
+# $ ./watch.sh
+##
+
+##
+# Run once in the beginning so we don't need to change a file
+# first to generate the static files for the first time.
+#
+printf '%s: Converting the files once\n' "$(date +'%F %T')"
+ruby ../adocwiki/src/main.rb "$PWD"
+
+##
+# The watch for .adoc file changes.
+#
+inotifywait -qmr -e modify --exclude '\.?#.*' docs/ \
+  | while read event; do
+      printf '%s: %s\n' "$(date +'%F %T')" "$event"
+      ruby ../adocwiki/src/main.rb "$PWD"
+    done
+
