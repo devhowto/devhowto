a9d18047c955f5f8c889ec8a1cb01b31395c69c7
Fernando Basso <fernandobasso.br@gmail.com>
2026-08-30 09:32:46 -0300

    Save notes on Sun, Aug the 30th, 2026 at 09:32

--- watch.sh
+++ watch.sh
@@ -18,7 +18,7 @@ cp -v ./docs/__assets/* ./build/__assets/
 ##
 # The watch for .adoc file changes.
 #
-inotifywait -qmr -e modify --exclude '\.?#.*' docs/ \
+inotifywait -qmr -e modify --exclude '\.?#.*' ./docs ../adocwiki \
   | while read event; do
       printf '%s: %s\n' "$(date +'%F %T')" "$event"
       ruby ../adocwiki/src/main.rb "$PWD"
