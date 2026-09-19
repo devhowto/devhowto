3e661340349ac890b9a2ac587ae5dd91cd946e26
Fernando Basso <fbassodev@gmail.com>
2026-09-19 10:08:49 -0300

    Save notes on Sat, Sep the 19th, 2026 at 10:08

--- watch.sh
+++ watch.sh
@@ -10,7 +10,7 @@
 # first to generate the static files for the first time.
 #
 printf '%s: Converting the files once\n' "$(date +'%F %T')"
-ruby ../adocwiki/src/main.rb "$PWD"
+ruby ../adwv1/src/main.rb "$PWD"
 
 mkdir -pv ./build/__assets
 cp -v ./docs/__assets/* ./build/__assets/
@@ -18,10 +18,10 @@ cp -v ./docs/__assets/* ./build/__assets/
 ##
 # The watch for .adoc file changes.
 #
-inotifywait -qmr -e modify --exclude '\.?#.*' ./nav.yml ./docs ../adocwiki \
+inotifywait -qmr -e modify --exclude '\.?#.*' ./nav.yml ./docs ../adwv1 \
   | while read event; do
       printf '%s: %s\n' "$(date +'%F %T')" "$event"
-      ruby ../adocwiki/src/main.rb "$PWD"
+      ruby ../adwv1/src/main.rb "$PWD"
       mkdir -pv ./build/__assets
       cp -v ./docs/__assets/* ./build/__assets/
     done
